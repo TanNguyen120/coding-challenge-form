50fd1dc22aa11108229a7fd7ac95808e573c01e4
Taan <tannguyen1220@gmail.com>
2024-01-06 08:03:50 +0700

    progress bar

---
+++ src/components/form.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FormProgress from './FormProgress';
+
+function Form() {
+  return (
+    <div className='w-[58.125rem] h-[28,25rem] flex flex-col items-center p-2 md:p-10 bg-[#F1F1F2] my-auto'>
+      <FormProgress />
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/components/formStage/FormStage1.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const FormStage1 = () => {
+  return <div>FormStage1</div>;
+};
+
+export default FormStage1;
